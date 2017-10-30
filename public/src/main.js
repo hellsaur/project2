f5bdcd01e736433cb4307707341b6e0581ab3521
marco acosta <marcucho14@hotmail.com>
2017-10-30 15:58:03 -0400

    got a glimpse of my api

---
+++ public/src/main.js
@@ -0,0 +1,16 @@
+function getSuperHero(e){
+  e.preventDefault();
+  fetch(`/superhero/${e.target.name.value}`)
+  .then(res => res.json())
+  .then(jsonRes => {
+    console.log(jsonRes);
+  })
+}
+
+function getHero(){
+  const form = document.querySelector('.container');
+  form.addEventListener('submit', (e) => getSuperHero(e));
+}
+
+
+document.addEventListener('DOMContentLoaded', getHero);
